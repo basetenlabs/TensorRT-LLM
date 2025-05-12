8e5c83dded8c3b0c12bb3c53006d9c02949ae51e
mahmoud <mahmoudhas@outlook.com>
2025-05-12 21:11:08 +0000

    review

--- tensorrt_llm/_torch/pyexecutor/guided_decoder.py
+++ tensorrt_llm/_torch/pyexecutor/guided_decoder.py
@@ -4,7 +4,6 @@ from typing import List, Optional
 
 import torch
 import xgrammar
-import json
 
 from ...bindings.executor import GuidedDecodingConfig, GuidedDecodingParams
 from .llm_request import LlmRequest
