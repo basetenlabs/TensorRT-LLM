36f12c481cdb9cadadc5d4af010acf31ee504c76
mahmoud <mahmoudhas@outlook.com>
2025-05-30 19:49:41 +0000

    ckpt

--- tensorrt_llm/_torch/pyexecutor/b10.py
+++ tensorrt_llm/_torch/pyexecutor/b10.py
@@ -42,6 +42,9 @@ class B10Decoder:
             cur_idx = next_idx
             next_idx += 1 + request.num_draft_tokens
     
+            if request.is_context_init_state and not request.is_last_context_chunk:
+                continue
+    
             is_custom = process_all_requests
 
             is_greedy = False
@@ -64,7 +67,7 @@ class B10Decoder:
                 is_mtp_disabled = True
                 is_custom = True
 
-            if temperature is not None and not math.isclose(temperature, B10Decoder.DEFAULT_TEMPERATURE):
+            if temperature is not None:
                 assert len(sampling_config.temperature) == 1
                 is_custom = True
                 if temperature < 1e-6:
